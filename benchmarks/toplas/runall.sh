bb9596f9b57c85ec82f3f7bc1c0f9d447e9457ed
Ian Briggs <ianbriggsutah@gmail.com>
2016-11-22 10:57:51 -0700

    Adding rosa-opt to the makefile and adding a run script

---
+++ benchmarks/toplas/runall.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+rm -f *log.txt
+
+make taylor-a           |& tee fptaylor-a-log.txt
+make taylor-b           |& tee fptaylor-b-log.txt
+make taylor-c           |& tee fptaylor-c-log.txt
+make taylor-c-tol       |& tee fptaylor-c-tol-log.txt
+make taylor-gelpia      |& tee fptaylor-gelpia-log.txt
+make taylor-gelpia-tol  |& tee fptaylor-gelpia-tol-log.txt
+
+make gappa              |& tee gappa-log.txt
+make gappa-hints        |& tee gappa-hints-log.txt
+make gappa-simple-hints |& tee gappa-simple-hints-log.txt
+
+rm -f generated/*
+rm -f vcs/*
+make rosa               |& tee rosa-log.txt
+rm -f generated/*
+rm -f vcs/*
+make rosa-opt           |& tee rosa-opt-log.txt
+
+
+
+
+make extra-taylor-a           |& tee extra-fptaylor-a-log.txt
+make extra-taylor-b           |& tee extra-fptaylor-b-log.txt
+make extra-taylor-c           |& tee extra-fptaylor-c-log.txt
+make extra-taylor-c-tol       |& tee extra-fptaylor-c-tol-log.txt
+make extra-taylor-gelpia      |& tee extra-fptaylor-gelpia-log.txt
+make extra-taylor-gelpia-tol  |& tee extra-fptaylor-gelpia-tol-log.txt
+
+make extra-gappa              |& tee extra-gappa-log.txt
+make extra-gappa-hints        |& tee extra-gappa-hints-log.txt
+make extra-gappa-simple-hints |& tee extra-gappa-simple-hints-log.txt
+
+rm -f generated/*
+rm -f vcs/*
+make extra-rosa               |& tee extra-rosa-log.txt
+rm -f generated/*
+rm -f vcs/*
+make extra-rosa-opt           |& tee extra-rosa-opt-log.txt
