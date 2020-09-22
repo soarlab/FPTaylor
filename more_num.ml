c0414d72f233a9c5f785ae70330aecd7e4468078
Alexey Solovyev <solovyev.alexey@gmail.com>
2020-09-21 21:31:39 -0700

    more_num: string_of_float_lo/hi

--- more_num.ml
+++ more_num.ml
@@ -104,43 +104,113 @@ let num_of_float x =
      let t = Int64.of_float (ldexp m 53) in
      num_of_big_int (Big_int.big_int_of_int64 t) */ (Int 2 **/ Int (e - 53))
 
-let log_num_simple b v =
-  let rec loop t k =
-    if t >/ v then k
-    else loop (Int b */ t) (k + 1) in
-  loop (Int 1) (-1)
+let log_big_int_floor b v =
+  let open Big_int in
+  let b = big_int_of_int b in
+  let rec loop t b' k k'  =
+    let t' = mult_big_int b' t in
+    if gt_big_int t' v then
+      if k' = 1 then k
+      else loop t b k 1
+    else loop t' (mult_big_int b' b) (k + k') (k' + 1) in
+  if sign_big_int v <= 0 then -1
+  else loop unit_big_int b 0 1
+
+let string_of_pos_finite_float_lo prec x =
+  assert (x > 0. && prec > 0);
+  let open Big_int in
+  let m, exp = frexp x in
+  let m, exp = big_int_of_int64 (Int64.of_float (ldexp m 53)), exp - 53 in
+  let two_exp = shift_left_big_int unit_big_int (abs exp) in
+  let ten = big_int_of_int 10 in
+  let n, rem =
+    if exp >= 0 then
+      shift_left_big_int m exp, zero_big_int
+    else
+      let mask = pred_big_int two_exp in
+      shift_right_big_int m (-exp), and_big_int m mask in
+  let r, e =
+    if sign_big_int n > 0 then
+      let k = log_big_int_floor 10 n + 1 in
+      let e = k - prec in
+      let b = power_big_int_positive_int ten (abs e) in
+      if e >= 0 then
+        div_big_int n b, e
+      else
+        let t = mult_big_int rem b in
+        let x = shift_right_big_int t (-exp) in
+        add_big_int (mult_big_int n b) x, e
+    else
+      let k = log_big_int_floor 10 (div_big_int two_exp rem) in
+      let b = power_big_int_positive_int ten (k + prec) in
+      let t = mult_big_int rem b in
+      let r = shift_right_big_int t (-exp) in
+      r, -(k + prec) in
+  let s = string_of_big_int r in
+  let e' = e + prec - 1 in
+  let s' = String.sub s 0 1 ^ "." ^ String.sub s 1 (prec - 1) in
+  if e' = 0 then s'
+  else s' ^ (if e' > 0 then "e+" else "e") ^ string_of_int e'
 
 let string_of_pos_finite_float_hi prec x =
   assert (x > 0. && prec > 0);
-  let q = num_of_float x in
-  let n = floor_num q in
-  if sign_num n > 0 then
-    let k = log_num_simple 10 n + 1 in
-    let r, e =
-      if k >= prec then
-        let b = Int 10 **/ Int (k - prec) in
-        let t = floor_num (n // b) in
-        t, k - prec
+  let open Big_int in
+  let m, exp = frexp x in
+  let m, exp = big_int_of_int64 (Int64.of_float (ldexp m 53)), exp - 53 in
+  let two_exp = shift_left_big_int unit_big_int (abs exp) in
+  let mask = pred_big_int two_exp in
+  let ten = big_int_of_int 10 in
+  let n, rem =
+    if exp >= 0 then
+      shift_left_big_int m exp, zero_big_int
+    else
+      shift_right_big_int m (-exp), and_big_int m mask in
+  let r, e, flag =
+    if sign_big_int n > 0 then
+      let k = log_big_int_floor 10 n + 1 in
+      let e = k - prec in
+      let b = power_big_int_positive_int ten (abs e) in
+      if e >= 0 then
+        let r, v = quomod_big_int n b in
+        r, e, sign_big_int v <> 0 || sign_big_int rem <> 0
       else
-        let b = Int 10 **/ Int (prec - k) in
-        n */ b +/ floor_num ((q -/ n) */ b), k - prec in
-    let r = if r */ (Int 10 **/ Int e) <=/ q then succ_num r else r in
+        let t = mult_big_int rem b in
+        let x, v = shift_right_big_int t (-exp), and_big_int t mask in
+        add_big_int (mult_big_int n b) x, e, sign_big_int v <> 0
+    else
+      let k = log_big_int_floor 10 (div_big_int two_exp rem) in
+      let b = power_big_int_positive_int ten (k + prec) in
+      let t = mult_big_int rem b in
+      let r, v = shift_right_big_int t (-exp), and_big_int t mask in
+      r, -(k + prec), sign_big_int v <> 0 in
+  let r = if flag then succ_big_int r else r in
+  let s = string_of_big_int r in
+  let s, e = 
+    if String.length s > prec then 
+      String.sub s 0 prec, succ e 
+    else s, e in
+  let e' = e + prec - 1 in
+  let s' = String.sub s 0 1 ^ "." ^ String.sub s 1 (prec - 1) in
+  if e' = 0 then s'
+  else s' ^ (if e' > 0 then "e+" else "e") ^ string_of_int e'
 
+let string_of_float_hi prec f =
+  match classify_float f with
+  | FP_infinite -> if f > 0. then "+inf" else "-inf"
+  | FP_nan -> "nan"
+  | FP_zero -> "0.0"
+  | _ -> 
+    if f > 0. then string_of_pos_finite_float_hi prec f
+    else "-" ^ string_of_pos_finite_float_lo prec (-.f)
 
-
-
-(* let string_of_pos_finite_float_hi prec x =
-  assert (x >= 0. && prec > 0);
-  let s = Printf.sprintf "%.*e" prec x in
-  let nx = num_of_float x in
-  let f = decode_num_str s in
-  let nf = num_of_gen_float f in
-  if nf >=/ nx then s
-  else
-    let f = {f with significand = succ_num f.significand} in
-    assert (num_of_gen_float f >=/ nx);
-
-    s *)
+let string_of_float_lo prec f =
+  match classify_float f with
+  | FP_infinite -> if f > 0. then "+inf" else "-inf"
+  | FP_nan -> "nan"
+  | FP_zero -> "0.0"
+  | _ -> 
+    if f > 0. then string_of_pos_finite_float_lo prec f
+    else "-" ^ string_of_pos_finite_float_hi prec (-.f)
 
 let is_exact str =
   let f = float_of_string str in
@@ -172,7 +242,7 @@ let next_float x =
        else
          Int64.float_of_bits (Int64.succ bits)
      end
-                               
+
 let prev_float x =
   match classify_float x with
   | FP_nan -> nan
